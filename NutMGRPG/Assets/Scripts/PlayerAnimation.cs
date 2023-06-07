79804c240805ffd6a48ce94b833cd960ef19ee7e
SolomonKI12 <SolomonPKI@gmail.com>
2023-06-07 14:30:31 +0100

    Another failed animation attempt

--- NutMGRPG/Assets/Scripts/PlayerAnimation.cs
+++ NutMGRPG/Assets/Scripts/PlayerAnimation.cs
@@ -5,21 +5,36 @@ using UnityEngine;
 public class PlayerAnimation : MonoBehaviour
 {
     public Animator animator;  // Reference to the Animator component
+    public DeathKnightBehaviour deathKnightBehaviour; // Reference to the DeathKnightBehaviour script
 
     void Update()
     {
-        float verticalInput = Input.GetAxisRaw("Vertical");
-        bool isMoving = Input.GetAxisRaw("Horizontal") != 0f || verticalInput != 0f;
-        bool isMouseInBottomHalf = Input.mousePosition.y < Screen.height / 2;
-        bool isMovingDown = verticalInput < 0f;
-        bool isMovingUp = verticalInput > 0f;
-        bool isFiring = Input.GetMouseButton(0) || Input.GetMouseButton(1);
+        bool isDead = deathKnightBehaviour.currentHealth <= 0; // Check if player has zero health
 
-        bool movingDown = (isMovingDown && !isFiring) || (isMoving && isFiring && isMouseInBottomHalf);
-        bool movingUp = (isMovingUp && !isFiring) || (isMoving && isFiring && !isMouseInBottomHalf);
+        if (!isDead)
+        {
+            float verticalInput = Input.GetAxisRaw("Vertical");
+            bool isMoving = Input.GetAxisRaw("Horizontal") != 0f || verticalInput != 0f;
+            bool isMouseInBottomHalf = Input.mousePosition.y < Screen.height / 2;
+            bool isMovingDown = verticalInput < 0f;
+            bool isMovingUp = verticalInput > 0f;
+            bool isFiring = Input.GetMouseButton(0) || Input.GetMouseButton(1);
 
-        // Set the animator parameters based on the input
-        animator.SetBool("MovingDown", movingDown);
-        animator.SetBool("MovingUp", movingUp);
+            bool movingDown = (isMovingDown && !isFiring) || (isMoving && isFiring && isMouseInBottomHalf);
+            bool movingUp = (isMovingUp && !isFiring) || (isMoving && isFiring && !isMouseInBottomHalf);
+
+            // Set the animator parameters based on the input
+            animator.SetBool("MovingDown", movingDown);
+            animator.SetBool("MovingUp", movingUp);
+        }
+        else
+        {
+            // Reset animator parameters if player has zero health
+            animator.SetBool("MovingDown", false);
+            animator.SetBool("MovingUp", false);
+        }
+
+        // Set the IsDead animator parameter
+        animator.SetBool("IsDead", isDead);
     }
 }
